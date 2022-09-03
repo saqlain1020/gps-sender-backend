c9758f95b164afa051baf16bfe9448d3c73ead53
Saqlain <saqlainprinters@gmail.com>
2022-09-03 18:44:05 +0500

    Delete no route fix

--- routes/devicesRouter.ts
+++ routes/devicesRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addDevice, getDevices } from "../controllers/deviceController";
+import { addDevice, deleteDevice, getDevices } from "../controllers/deviceController";
 
 const deviceRouter = Router();
 
 deviceRouter.get("/", getDevices);
 deviceRouter.post("/", addDevice);
+deviceRouter.delete("/", deleteDevice);
 
 export default deviceRouter;
