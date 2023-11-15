baa39997685e5d003888cf31012839fd21acf99e
Saqlain <saqlainprinters@gmail.com>
2023-11-15 11:20:23 +0500

    feat: update

--- routes/busRouter.ts
+++ routes/busRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { addBus, deleteBus, getBusById, getBuses } from "../controllers/busController";
+import { protect } from "../controllers/userController";
 
 const busRouter = Router();
 
 busRouter.get("/", getBuses);
-busRouter.delete("/", deleteBus);
+busRouter.delete("/", protect, deleteBus);
 busRouter.get("/:id", getBusById);
 busRouter.post("/", addBus);
 
